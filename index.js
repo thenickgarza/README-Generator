d676283c5e0e6776213cc5b0b7849e9fc7557907
Nick Garza <thenickgarza14@gmail.com>
2022-01-13 00:52:25 -0600

    Have all the questions implemented

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name? (Required)',
+            message: 'Please enter your projects Name (Required)',
             validate: nameInput => {
                 if (nameInput) {
                   return true;
@@ -20,20 +20,92 @@ const promptUser = () => {
         },
         {
             type: 'input',
-            name: 'github',
-            message: 'What is your github username?',
-            validate: githubInput => {
-                if (githubInput) {
+            name: 'description',
+            message: 'Please describe the project',
+            validate: descriptionInput => {
+                if (descriptionInput) {
                     return true;
                 } else {
-                    console.log('Please enter a github username');
+                    console.log('You need to provide a description to continue!');
                     return false;
                 }
             }
         },
+        {
+            type: 'confirm',
+            name: 'confirmTableOfContents',
+            message: 'Would you like to include a Table Of Contents in your README.md?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please describe the steps required to install your project',
+            validate: installationInput => {
+                if (installationInput) {
+                    return true;
+                } else {
+                    console.log("You need to let people know how to install your projec to continue!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please decribe the projects intended usage in the world',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log("You need to let people know your projects intended usage to continue");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'inout',
+            name: 'credits',
+            message: 'Please provide the collaborators, if any, with their Github profiles',
+            validate: creditInput => {
+                if (creditInput) {
+                    return true;
+                } else {
+                    console.log("You need to provide the credits for the project to continue");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'Please choose a license for the project',
+            choices: ['BSD', 'MIT', 'GPL'],
+            validate: licenseInput => {
+                if (licenseInput) {
+                    return true;
+                } else {
+                    console.log('Please choose a license for the project to continue');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: "Plese provide some guidelines for other devleopers to contribute to the project"
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Provide examples on how to run tests you created for the application'
+        }
     ]);
 };
 promptUser()
+.then(data => {
+    console.log(data);
+})
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
